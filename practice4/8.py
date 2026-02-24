5a53c6d8866eca310b08248f7428567a8a0bc39d
Рахман <oopmrarar@gmail.com>
2026-02-24 22:24:55 +0500

    task 8

---
+++ practice4/8.py
@@ -0,0 +1,14 @@
+def primes(n):
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+
+
+n = int(input())
+
+print(" ".join(str(x) for x in primes(n)))
